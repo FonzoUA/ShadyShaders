8a25c5137d0d0c157de56a705329aa1fe3c5e988
vyanchuk <fonzoua@gmail.com>
2018-07-14 21:06:16 -0600

    Played around with some basic math stuff, nothing cool really

---
+++ ShadyShader/Assets/SampleCodes/Clock Thingy/Clock.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+    private float degreePerHour = 30.0f;
+    private float degreePerMinute;
+    private float degreePerSecond;
+
+    private void Awake()
+    {
+        degreePerMinute = 6.0f;
+        degreePerSecond = 6.0f;
+    }
+
+    private void Update()
+    {
+        System.DateTime time = System.DateTime.Now;
+
+        hoursTransform.localRotation = Quaternion.Euler(0, time.Hour * degreePerHour, 0);
+        minutesTransform.localRotation = Quaternion.Euler(0, time.Minute * degreePerMinute, 0);
+        secondsTransform.localRotation = Quaternion.Euler(0, time.Second * degreePerSecond, 0);
+    }
+
+}
---
+++ ShadyShader/Assets/SampleCodes/Graph Thingy/Graph.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform point;
+    [Range(10, 1000)] public int numPoints;
+    private Transform[] temp;
+
+    private void Awake()
+    {
+        temp = new Transform[numPoints];
+        float step = 2f / numPoints;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0f;
+        position.z = 0;
+        for (int i = 0; i < numPoints; i++)
+        {
+            temp[i] = Instantiate(point);
+            position.x = (i + 0.5f) * step - 1f;
+            temp[i].localPosition = position;
+            temp[i].localScale = scale;
+            temp[i].SetParent(transform, false);
+        }
+    }
+
+    private void Update()
+    {
+        for (int i = 0; i < numPoints; i++)
+        {
+            Transform t = temp[i];
+            Vector3 position = t.localPosition;
+            position.y = Mathf.Cos(Mathf.PI * position.x + Time.time) ;
+            t.localPosition = position;
+        }
+    }
+
+}
