5874e53f21992332ff063bc6e9ad87bd2bf595e0
vyanchuk <fonzoua@gmail.com>
2018-07-16 07:10:13 -0600

    FPS display + stats averaging

---
+++ ShadyShader/Assets/SampleCodes/FPS Thingy/SpawnFPS.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFPS : MonoBehaviour
+{
+    public float spawnInterval;
+    public float spawnDistance;
+    public TestFPS[] objectPrefabs;
+
+    [Header("Objects spawned")] public float totalObjects = 0;
+
+    private float elapsedTime;
+
+    private void FixedUpdate()
+    {
+        elapsedTime += Time.deltaTime;
+        if (elapsedTime >= spawnInterval)
+        {
+            elapsedTime -= spawnInterval;
+            Spawn();
+            totalObjects++;
+        }
+    }
+
+    private void Spawn()
+    {
+        TestFPS temp = objectPrefabs[Random.Range(0, objectPrefabs.Length)];
+        TestFPS spawn = Instantiate<TestFPS>(temp);
+        spawn.transform.localPosition = Random.onUnitSphere * spawnDistance;
+    }
+}
---
+++ ShadyShader/Assets/SampleCodes/FPS Thingy/TestFPS.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class TestFPS : MonoBehaviour
+{
+    public float attractionForce;
+
+    private Rigidbody rigbod;
+
+    private void Awake()
+    {
+        rigbod = this.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigbod.AddForce(transform.localPosition * -attractionForce);
+    }
+}
